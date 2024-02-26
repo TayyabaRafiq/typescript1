d3efe3ad4303996163144ee9f632fff6a66d877f
TayyabaRafiq <161163932+TayyabaRafiq@users.noreply.github.com>
2024-02-26 02:51:48 -0800

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,25 @@
+"use strict";
+var message = "1234";
+console.log(message);
+var messageNew = "5678";
+console.log(messageNew);
+// variable with let
+let favouriteColour = "blue";
+console.log(favouriteColour);
+//Data type string:
+let userName = "Arbish";
+console.log(userName);
+//Number:
+let age = 10;
+console.log(age);
+//boolean:
+let arbish_cute = true;
+console.log(arbish_cute);
+//Strong typing:
+let a = "pakistan";
+let b = 10;
+let is_the_best = true;
+console.log(a);
+console.log(b);
+console.log(is_the_best);
+console.log(a, b, is_the_best);
